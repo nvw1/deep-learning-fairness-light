f21646a56327ab8962a5b11232156b751a61ee1d
nvw1 <n.v.wenzler@gmail.com>
2021-08-10 16:55:12 +0200

    deleting opacus

--- opacusAboutToDelete/layers/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
-from .dp_lstm import DPLSTM
-from .dp_multihead_attention import DPMultiheadAttention, SequenceBias
-
-
-__all__ = ["DPLSTM", "DPMultiheadAttention", "SequenceBias"]
