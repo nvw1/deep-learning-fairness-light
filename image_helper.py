91affd0f8ee135729150aaf5e93fc580b46c43d1
nvw1 <n.v.wenzler@gmail.com>
2021-08-12 10:20:25 +0200

    fixed intent

--- image_helper.py
+++ image_helper.py
@@ -32,57 +32,57 @@ class ImageHelper(Helper):
 
     
     def load_celeba_data(self):
-    """Build and return a data loader."""
-    self.name = self.params['name']
-    
-    #TODO maybe not even needed? since instant use in the files?
-    #TODO wouldn't it be cleaner to initiallize it at the beginning and then just pass it rather then lookup?
-    image_dir = ''
-    attr_path = ''
-    selected_attrs = ''
-    
-    crop_size = 178 #178 #TODO maybe lower image resolution?
-    image_size = 128 #128 #TODO pixels find me
-    
-    flip = transforms.RandomHorizontalFlip()
-    crop = transforms.CenterCrop(crop_size)
-    resize = transforms.Resize(image_size)
-    normalize = transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-    
-    transform_train = transforms.Compose([flip, crop, resize, transforms.ToTensor(), normalize])
-    transform_test = transforms.Compose([crop, resize, transforms.ToTensor(), normalize])
+        """Build and return a data loader."""
+        self.name = self.params['name']
+        
+        #TODO maybe not even needed? since instant use in the files?
+        #TODO wouldn't it be cleaner to initiallize it at the beginning and then just pass it rather then lookup?
+        image_dir = ''
+        attr_path = ''
+        selected_attrs = ''
+        
+        crop_size = 178 #178 #TODO maybe lower image resolution?
+        image_size = 128 #128 #TODO pixels find me
+        
+        flip = transforms.RandomHorizontalFlip()
+        crop = transforms.CenterCrop(crop_size)
+        resize = transforms.Resize(image_size)
+        normalize = transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+        
+        transform_train = transforms.Compose([flip, crop, resize, transforms.ToTensor(), normalize])
+        transform_test = transforms.Compose([crop, resize, transforms.ToTensor(), normalize])
 
-    self.train_dataset = CelebADataset(image_dir=self.params['image_dir'],
-                                        attr_path=self.params['attr_path'],
-                                        selected_attr=self.params['selected_attr'],
-                                        protected_attr=self.params['protected_attr'],
-                                        mode='train',
-                                        transform=transform_train)
+        self.train_dataset = CelebADataset(image_dir=self.params['image_dir'],
+                                            attr_path=self.params['attr_path'],
+                                            selected_attr=self.params['selected_attr'],
+                                            protected_attr=self.params['protected_attr'],
+                                            mode='train',
+                                            transform=transform_train)
 
-    self.test_dataset = CelebADataset(image_dir=self.params['image_dir'],
-                                        attr_path=self.params['attr_path'],
-                                        selected_attr=self.params['selected_attr'],
-                                        protected_attr=self.params['protected_attr'],
-                                        mode='test',
-                                        transform=transform_test)
-    
-    self.dataset_size = len(self.train_dataset)
-    logger.info(f"Length of CelebA dataset: {self.dataset_size}")
-    
-    #Maybe the different shuffles make the total a different mix?
-    self.train_loader = torch.utils.data.DataLoader(self.train_dataset,
-                                                    batch_size=self.params['batch_size'],
-                                                    shuffle=True,
-                                                    num_workers=2,
-                                                    drop_last=True)
+        self.test_dataset = CelebADataset(image_dir=self.params['image_dir'],
+                                            attr_path=self.params['attr_path'],
+                                            selected_attr=self.params['selected_attr'],
+                                            protected_attr=self.params['protected_attr'],
+                                            mode='test',
+                                            transform=transform_test)
+        
+        self.dataset_size = len(self.train_dataset)
+        logger.info(f"Length of CelebA dataset: {self.dataset_size}")
+        
+        #Maybe the different shuffles make the total a different mix?
+        self.train_loader = torch.utils.data.DataLoader(self.train_dataset,
+                                                        batch_size=self.params['batch_size'],
+                                                        shuffle=True,
+                                                        num_workers=2,
+                                                        drop_last=True)
 
-    self.test_loader = torch.utils.data.DataLoader(self.test_dataset,
-                                                    batch_size=self.params['test_batch_size'],
-                                                    shuffle=False, #TODO changed this to true for although what is the point...
-                                                    num_workers=2)
-            
-    self.labels = self.params['labels']
-    return True
+        self.test_loader = torch.utils.data.DataLoader(self.test_dataset,
+                                                        batch_size=self.params['test_batch_size'],
+                                                        shuffle=False, #TODO changed this to true for although what is the point...
+                                                        num_workers=2)
+                
+        self.labels = self.params['labels']
+        return True
 
 
 
