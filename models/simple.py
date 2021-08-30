8f97816edba52bdf60c2b0e89cc7e8602490ed75
nvw1 <n.v.wenzler@gmail.com>
2021-08-30 09:44:23 +0200

    Update simple.py

--- models/simple.py
+++ models/simple.py
@@ -1,3 +1,7 @@
+# Based on https://github.com/FarrandTom/deep-learning-fairness
+# from @article{farrand2020neither, title={Neither Private Nor Fair: Impact of Data Imbalance on Utility and Fairness in Differential Privacy}, author={Farrand, Tom and Mireshghallah, Fatemehsadat and Singh, Sahib and Trask, Andrew}, journal={arXiv preprint arXiv:2009.06389}, year={2020} }
+
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
